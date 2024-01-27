c23a5df6d81ea7801c0877f64bead71851494672
khrishchanovich <orannon@gmail.com>
2024-01-27 18:20:11 +0300

    Complete Lab1

--- Lab1/CaesarCipher/Program.cs
+++ Lab1/CaesarCipher/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+public class CaesarCipher
+{
+    const string alfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+    private string CodeEncode(string text, int k)
+    {
+        var fullAlfabet = alfabet + alfabet.ToLower();
+        var letterQty = fullAlfabet.Length;
+        var retVal = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            var c = text[i];
+            var isUpper = Char.IsUpper(c); 
+            var index = fullAlfabet.IndexOf(Char.ToUpper(c));
+            if (index < 0)
+            {
+                retVal += c.ToString();
+            }
+            else
+            {
+                var codeIndex = (letterQty + index + k) % letterQty;
+                retVal += isUpper ? Char.ToUpper(fullAlfabet[codeIndex]) : Char.ToLower(fullAlfabet[codeIndex]);
+            }
+        }
+
+        return retVal;
+    }
+
+
+    public string Encrypt(string plainMessage, int key)
+        => CodeEncode(plainMessage, key);
+
+    public string Decrypt(string encryptedMessage, int key)
+        => CodeEncode(encryptedMessage, -key);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var cipher = new CaesarCipher();
+
+        // Чтение текста из файла
+        string encryptedText;
+        try
+        {
+            encryptedText = File.ReadAllText(@"D:\6 semester\IN-SB\Lab1\CaesarCipher\encryptedtext.txt");
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Ошибка при чтении файла: " + e.Message);
+            return;
+        }
+
+        Console.WriteLine("Текст из файла:");
+        Console.WriteLine(encryptedText);
+        Console.WriteLine();
+
+        Console.Write("Введите значение ключа: ");
+        var secretKey = Convert.ToInt32(Console.ReadLine());
+
+        var encryptedMessage = cipher.Encrypt(encryptedText, secretKey);
+
+        Console.WriteLine("Зашифрованное сообщение: {0}", encryptedMessage);
+        Console.WriteLine("Расшифрованное сообщение: {0}", cipher.Decrypt(encryptedMessage, secretKey));
+        Console.ReadLine();
+
+    }
+}
+
--- Lab1/VigenereСipher/Program.cs
+++ Lab1/VigenereСipher/Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class VigenereCipher
+{
+    static char EncryptChar(char plainChar, char keyChar)
+    {
+        if (!char.IsLetter(plainChar))
+            return plainChar;
+
+        char baseChar = 'A';
+        return (char)(((plainChar + keyChar - 2 * baseChar) % 26) + baseChar);
+    }
+
+    static char DecryptChar(char cipherChar, char keyChar)
+    {
+        if (!char.IsLetter(cipherChar))
+            return cipherChar;
+
+        char baseChar = 'A';
+        int result = ((cipherChar - keyChar + 26) % 26) + baseChar;
+        return (char)result;
+    }
+
+    public static string Encrypt(string plaintext, string key)
+    {
+        plaintext = plaintext.ToUpper();
+        key = key.ToUpper(); 
+
+        string encryptedText = "";
+        int keyIndex = 0;
+
+        foreach (char c in plaintext)
+        {
+            if (char.IsWhiteSpace(c))
+            {
+                encryptedText += c;
+                continue;
+            }
+
+            char keyChar = key[keyIndex];
+            encryptedText += EncryptChar(c, keyChar);
+            keyIndex = (keyIndex + 1) % key.Length;
+        }
+
+        return encryptedText;
+    }
+
+    public static string Decrypt(string ciphertext, string key)
+    {
+        ciphertext = ciphertext.ToUpper(); 
+        key = key.ToUpper();
+
+        string decryptedText = "";
+        int keyIndex = 0;
+
+        foreach (char c in ciphertext)
+        {
+            if (char.IsWhiteSpace(c))
+            {
+                decryptedText += c;
+                continue;
+            }
+
+            char keyChar = key[keyIndex];
+            decryptedText += DecryptChar(c, keyChar);
+            keyIndex = (keyIndex + 1) % key.Length;
+        }
+
+        return decryptedText;
+    }
+
+
+    static void Main()
+    {
+        string encryptedText;
+        try
+        {
+            encryptedText = File.ReadAllText(@"D:\6 semester\IN-SB\Lab1\VigenereСipher\encryptedtext.txt");
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Ошибка при чтении файла: " + e.Message);
+            return;
+        }
+        Console.WriteLine("Текст из файла:");
+        Console.WriteLine(encryptedText);
+        Console.WriteLine();
+
+        Console.Write("Введите значение ключа: ");
+        var secretKey = Console.ReadLine();
+
+        string ciphertext = Encrypt(encryptedText, secretKey);
+        Console.WriteLine("Encrypted: " + ciphertext);
+
+        string decryptedText = Decrypt(ciphertext, secretKey);
+        Console.WriteLine("Decrypted: " + decryptedText);
+    }
+}
